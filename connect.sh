3f33ba2e17201fc516eed73ae0d1172aa6f2d694
tangyulin001 <285527665@qq.com>
2024-03-21 18:53:51 +0800

    third commit

--- connect.sh
+++ connect.sh
@@ -5,7 +5,17 @@
 read -p "请输入节点的编号：" number
 read -p "请输入节点的ip地址：" ipaddr
 
-gnome-terminal --tab --title="node${number}" -- bash -c "echo Running node${number}; hraftd -id node${number} -haddr ${ipaddr}:11000 -raddr ${ipaddr}:12000 -join 10.211.55.16:11000 ~/node${number}; exec bash"
+if [${number} -gt 0]
+then
+  read -p "请输入要加入的节点的ip地址：" join_ip
+  gnome-terminal --tab --title="node${number}" -- bash -c "echo Running node${number}; hraftd -id node${number} -haddr ${ipaddr}:11000 -raddr ${ipaddr}:12000 -join ${join_ip}:11000 ~/node${number}; exec bash"
+elif [${number} -eq 0]
+then
+  gnome-terminal --tab --title="node0" -- bash -c "echo Running node0; hraftd -id node0 -haddr ${ipaddr}:11000 -raddr ${ipaddr}:12000 ~/node0; exec bash"
+else
+  echo "请输入非负整数"
+fi
+  
 
 
 
--- startnode0.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-gnome-terminal --tab --title="node0" -- bash -c "echo Running node0; hraftd -id node0 -haddr 10.211.55.16:11000 -raddr 10.211.55.16:12000 ~/node0; exec bash"
