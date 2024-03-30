954088807e6171d0bd5df3dac185625aaba6bf04
tangyulin001 <285527665@qq.com>
2024-03-30 13:19:20 +0800

    update

--- deploy.sh
+++ deploy.sh
@@ -1,68 +1,11 @@
 #!/bin/bash
 
-mkdir work 
-cd work
-export GOPATH=$PWD
-mkdir -p src/github.com/otoolep
-cd src/github.com/otoolep/
-git clone git@github.com:otoolep/hraftd.git
-cd hraftd
-git clone https://github.com/hashicorp/raft.git
-echo 'replace github.com/hashicorp/raft => ./raft' >> go.mod
-go mod tidy
-go install
-cd ../../../../bin
-export PATH=$PATH:$PWD
-cd ../../
-# 检查MySQL是否已安装
-if ! dpkg -l | grep -q mysql-server; then
-    echo "MySQL未安装，将执行更新并安装MySQL..."
-    # 更新apt-get源并安装MySQL
-    sudo apt-get update
-    sudo apt-get install mysql-server -y
-    sudo ufw allow mysql
-else
-    echo "MySQL已经安装，无需执行安装操作。"
-fi
-
-# 提示用户输入新密码和旧密码
-read -sp "Enter new MySQL root password: " new_password
-read -sp "Enter old MySQL root password: " old_password
-
-# 修改 MySQL 密码
-sudo mysql -u root -p${old_password} -e "ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY '$new_password'; FLUSH PRIVILEGES;"
-
-# 输出结果
-if [ $? -eq 0 ]; then
-    echo "MySQL root password updated successfully."
-    sudo systemctl restart mysql
-else
-    echo "Failed to update MySQL root password."
-fi
-
-# 修改 MySQL root 用户的主机
-sudo mysql -u root -p"${new_password}" -e "UPDATE mysql.user SET Host='%' WHERE User='root'; FLUSH PRIVILEGES;"
-
-# 检查命令执行结果并输出消息
-if [ $? -eq 0 ]; then
-    echo "MySQL root user host updated successfully."
-else
-    echo "Failed to update MySQL root user host."
-fi
-
-# 下载数据集
 wget -O openmrs-2-2-1.sql.gz "https://openmrs.atlassian.net/wiki/download/attachments/26273323/openmrs-2-2-1.sql.gz?api=v2"
-
-# 解压缩文件
 gunzip openmrs-2-2-1.sql.gz
-
-# 导入数据到 MySQL 数据库
-mysql -u root -p"$mysql_password" -e "CREATE DATABASE IF NOT EXISTS openmrs;"
-mysql -u root -p"$mysql_password" openmrs < openmrs-2-2-1.sql
-
-# 创建视图
-mysql -u root -p"$mysql_password" <<EOF
-USE openmrs;
+docker run -d -p 3306:3306 -e MYSQL_ROOT_PASSWORD=root1234 --name mysql-ct mysql:latest
+docker cp openmrs-2-2-1.sql mysql_container:.
+docker exec -it mysql_container sh -c 'mysql -uroot -p"root1234" -e "CREATE DATABASE IF NOT EXISTS foo;";mysql -u root -p"root1234" foo < openmrs-2-2-1.sql;mysql -u root -p"root1234" <<EOF
+USE foo;
 CREATE VIEW Vedrfolnir AS
 SELECT
     o.obs_id,
@@ -78,34 +21,27 @@ JOIN
 JOIN
     concept_name cn ON o.concept_id = cn.concept_id;
 EOF
+mysql -u root -p"root1234" -e "USE foo;CREATE TABLE patient_data (patient_id INT,ciphertext BLOB,hash CHAR(64));"'
 
-# 检查命令执行结果并输出消息
-if [ $? -eq 0 ]; then
-    echo "View 'Vedrfolnir' created successfully."
-else
-    echo "Failed to create view 'Vedrfolnir'."
-fi
-
-#创建存放密文的表
-mysql -u root -p"$mysql_password" -e "CREATE TABLE patient_data (patient_id INT,ciphertext BLOB,hash CHAR(64));"
-
-# 检查命令执行结果并输出消息
-if [ $? -eq 0 ]; then
-    echo "TABLE patient_data created successfully."
-else
-    echo "Failed to create TABLE patient_data."
-fi
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+git clone https://github.com/HenryRuis/Merkle2Test.git
+sleep 3
+cd Merkle2Test
+touch Dockerfile
+chmod a+x Dockerfile
+echo 'FROM golang:latest
+WORKDIR /go/src/app
+ENV GOPROXY=https://goproxy.cn
+COPY . .
+RUN go run ./daemon/new.go > out.txt
+CMD ["cat","out.txt"]' > Dockerfile
+for i in {1..5}
+do
+sed "s/LIMIT 10000/LIMIT $((${i}*10000))/" ./daemon/main.go > ./daemon/new.go
+docker build --network=host -t "mt-${i}" .
+wait
+docker run --rm --net=host --name "mt-${i}" "mt-${i}" > "output${i}.txt"
+wait 
+#docker kill "mt-${i}"
+#docker rm -f "mt-${i}"
+docker rmi "mt-${i}" 
+done
--- gitcode.sh
+++ gitcode.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 git add .
-git commit -m "commit"
-git push -u origin main
+git commit -m "update"
+git push
