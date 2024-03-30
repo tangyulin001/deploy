ec0088cfc676b5c4daa4cc5e6e694289b21d5d0d
tangyulin001 <285527665@qq.com>
2024-03-31 00:13:47 +0800

    update

--- vf_c.sh
+++ vf_c.sh
@@ -5,9 +5,9 @@ do
 sed "s/LIMIT 100000/LIMIT $((${i}*100000))/" ./daemon/test_hmst.go > ./daemon/new.go
 docker build --network=host -t "vf-${i}" .
 sleep 30
-echo "LIMIT $((${i}*100000)):" >> ../vf-out.txt
+echo "LIMIT $((${i}*100000)):" >> ../../vf-out.txt
 sleep 1
-docker run --rm --net=host --name "vf-${i}" "vf-${i}" >> ../vf-out.txt
+docker run --rm --net=host --name "vf-${i}" "vf-${i}" >> ../../vf-out.txt
 sleep 10 
 docker rmi "vf-${i}" 
 done
