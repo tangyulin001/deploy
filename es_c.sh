0b35f7fc711dacd851fe75ec88744596abf5d73d
tangyulin001 <285527665@qq.com>
2024-04-03 01:39:35 +0800

    update

--- es_c.sh
+++ es_c.sh
@@ -14,12 +14,12 @@ if [ $? -eq 0 ]; then
 successln "docker es_${i} built successfully."
 fi
 
-echo "" >> ../output/es_out.txt
-echo "第${n}次测试..." >> ../output/es_out.txt
-echo "LIMIT $((${i}*100000)):" >> ../output/es_out.txt
+echo "" >> ../output/es.out
+echo "第${n}次测试..." >> ../output/es.out
+echo "LIMIT $((${i}*100000)):" >> ../output/es.out
 
-#write results to ../output/es_out.txtes_out.txt
-docker run --rm --net=host --name "es_${i}" "es_${i}" >> ../output/es_out.txt
+#write results to ../output/es.outes.out
+docker run --rm --net=host --name "es_${i}" "es_${i}" >> ../output/es.out
 
 infoln "in condition of LIMIT $((${i}*100000)),the number of patient_data is："
 mysql -uroot -p"root1234" foo -e "select count(*) from patient_data;"
